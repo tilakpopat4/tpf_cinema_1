f3f96d0ea01310026bc76e6a78227a7cb72de810
Tilak Popat <tilakpopat2007@gmail.com>
2026-06-22 16:23:32 +0530

    Fix Google Service Account private key decoder bug

--- src/lib/gdrive.ts
+++ src/lib/gdrive.ts
@@ -5,10 +5,19 @@ export async function getDriveAccessToken() {
     throw new Error('Google Cloud Service Account credentials are not configured in environment variables.');
   }
 
+  let privateKey = process.env.GOOGLE_PRIVATE_KEY;
+  if (privateKey.startsWith('"') && privateKey.endsWith('"')) {
+    privateKey = privateKey.slice(1, -1);
+  }
+  if (privateKey.startsWith("'") && privateKey.endsWith("'")) {
+    privateKey = privateKey.slice(1, -1);
+  }
+  privateKey = privateKey.replace(/\\n/g, '\n');
+
   const auth = new GoogleAuth({
     credentials: {
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      private_key: privateKey,
     },
     scopes: ['https://www.googleapis.com/auth/drive.file'],
   });
